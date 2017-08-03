f0ca6b514497fca22b6eb3958829c4b910941a72
funafunfa <golovolomka132@gmail.com>
2017-08-03 15:21:03 +0300

    Cleared all the mess. Done for the args.
    TODO 03.08.17
       Make a little GUI.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,33 +1,75 @@
 package main;
 
+import javax.imageio.ImageIO;
 import javax.print.*;
-import javax.print.attribute.HashPrintRequestAttributeSet;
-import javax.print.attribute.PrintRequestAttributeSet;
-import javax.print.attribute.standard.Copies;
-import javax.swing.*;
 import net.sourceforge.barbecue.BarcodeFactory;
 import net.sourceforge.barbecue.Barcode;
 import net.sourceforge.barbecue.BarcodeException;
 import net.sourceforge.barbecue.BarcodeImageHandler;
-import net.sourceforge.barbecue.formatter.BarcodeFormatter;
 import net.sourceforge.barbecue.output.OutputException;
-
+import usseless.TextToGraphicConverter;
 import java.awt.image.BufferedImage;
 import java.awt.*;
 import java.io.*;
+
+import static main.doublingPNGs.doubleBufferedImage;
+import static main.mergingPNGs.joinBufferedImage;
+
 public class Main {
 
-    public static String argument1, argument2, argument3;
+    public static String firstText, textForBarcode, problem, barcodeFileName, combinedFileName;
 // For catching and storing arguments
 
 
-    public static void main(String args[]) throws BarcodeException, OutputException {
-//        usingBarbecueAsSwingComponent(args[0]);
-//        drawingBarcodeDirectToGraphics(args[0]);
-        argument1 = args[0];
-        argument2 = args[1];
-        argument3 = args[2];
-        outputtingBarcodeAsPNG(text);
+    public static void main(String args[]) throws BarcodeException, OutputException, ArrayIndexOutOfBoundsException {
+        try {
+            firstText = args[0];
+            textForBarcode = args[1];
+            problem = args[2];
+
+
+            outputtingBarcodeAsPNG(textForBarcode);
+
+            System.out.println("BarCode name: " + barcodeFileName);
+
+            BufferedImage firstTextImage = new TextToGraphicConverter().convertTextToGraphic(firstText, new Font("Arial", Font.PLAIN, 24));
+            ImageIO.write(firstTextImage, "png", new File(firstText + ".png"));
+
+            System.out.println("Making firstTextImage picture: "+ firstText + ".png");
+            BufferedImage problemImage = new TextToGraphicConverter().convertTextToGraphic(problem, new Font("Arial", Font.PLAIN, 24));
+
+            System.out.println("Making problemImage picture: "+ problem + ".png");
+            ImageIO.write(problemImage, "png", new File(problem + ".png"));
+
+            BufferedImage img1 = ImageIO.read(new File(firstText + ".png"));
+            BufferedImage img2 = ImageIO.read(new File(barcodeFileName));
+            BufferedImage img3 = ImageIO.read(new File(problem + ".png"));
+            BufferedImage joinedImg = joinBufferedImage(img1,img2, img3);
+
+            combinedFileName = firstText + ".png";
+
+            boolean success = ImageIO.write(joinedImg, "png", new File(combinedFileName));
+
+            if (success) System.out.println("Combined picture was made: " + combinedFileName);
+
+            BufferedImage combinedImage = ImageIO.read(new File(combinedFileName));
+            BufferedImage doubledImage = doubleBufferedImage(combinedImage, combinedImage);
+            // TODO: 8/3/2017 Change paths
+            // TODO: 8/3/2017 Make normal logs for nerds
+            success = ImageIO.write(doubledImage, "png", new File(combinedFileName));
+
+            if (success) System.out.println("Doubled and Combined picture was made: " + combinedFileName);
+
+            PrintService service = PrintServiceLookup.lookupDefaultPrintService();
+            DocPrintJob job = service.createPrintJob();
+            DocFlavor flavor = DocFlavor.SERVICE_FORMATTED.PRINTABLE;
+            SimpleDoc doc = new SimpleDoc(new oneMoreTimeBabe.MyPrintable(), flavor, null);
+            job.print(doc, null);
+            System.out.println("Sent to the: " + service.getName());
+
+        } catch (IOException | PrintException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void outputtingBarcodeAsPNG(String given_data) throws BarcodeException {
@@ -37,20 +79,10 @@ public class Main {
             File f = new File(given_data + ".png");
             System.out.println("Creating file");
 
-
             // Let the barcode image handler do the hard work
             BarcodeImageHandler.savePNG(barcode, f);
             System.out.println("Making a BarCode");
-
-
-
-
-//            Graphics g = f;
-//            g.setFont(g.getFont().deriveFont(30f));
-//            g.drawString("Hello World!", 100, 100);
-//            g.dispose();
-
-
+            barcodeFileName = given_data + ".png";
         } catch (Exception e) {
             // Error handling here
         }
--- src/main/doublingPNGs.java
+++ src/main/doublingPNGs.java
@@ -1,32 +1,22 @@
 package main;
 
-import javax.imageio.ImageIO;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+
 
 
 public class doublingPNGs {
-    public static void main(String[] args) {
-        try {
-            BufferedImage img1 = ImageIO.read(new File("joined.png"));
-            BufferedImage joinedImg = joinBufferedImage(img1,img1);
-            joinedImg.getScaledInstance(10,10,Image.SCALE_DEFAULT);
-            // TODO: 8/3/2017 Change paths
-            // TODO: 8/3/2017 Make normal logs for nerds
-            boolean success = ImageIO.write(joinedImg, "png", new File("doubled.png"));
-            System.out.println("saved success? "+success);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-    public static BufferedImage joinBufferedImage(BufferedImage img1, BufferedImage img2) {
+
+    public static BufferedImage doubleBufferedImage(BufferedImage img1, BufferedImage img2) {
 
         //do some calculate first
+        /*
+
+        Some weird magic
+
+        */
         int offset  = 100;
-//        int wid = img1.getWidth()+img2.getWidth()+offset;
         int wid =img1.getWidth();
         int height =(img1.getHeight() *2) + offset ;
         //create a new buffer and draw two image into the new image
